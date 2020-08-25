9adea70ed76bdf73c681fd4c5e13dce5bc0b5d88
utsav-goel <69675502+utsav-goel@users.noreply.github.com>
2020-08-25 20:27:12 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class Triangle {
+
+	@Test
+	public void angle() {
+		// TODO Auto-generated method stub
+		double base = 20.0;
+	      double height = 110.5;
+	      double area = (base* height)/2;
+	      System.out.println("Area of Triangle is: " + area); 
+	}
+
+}
